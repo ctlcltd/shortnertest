e5ad539370f72d12e2f511e2fa773dccf2ad29ce
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2020-07-06 21:14:15 +0200

    06-07-2020 21:14:15

--- urls/Virtual.php
+++ urls/Virtual.php
@@ -14,26 +14,26 @@ use \framework\DatabaseSQL;
 
 class Virtual extends DatabaseSQL {
 	public function fetch(string $collection, $keys = NULL, bool $single = false, bool $distinct = false) {
-		$collection = \urls\COLLECTIONS_TEMPLATE[$collection]['table'];
+		$collection = \urls\COLLECTIONS_TEMPLATE[$collection]['source'];
 
 		parent::fetch($collection, $keys, $single, $distinct);
 	}
 
 	public function add(string $collection) {
-		$collection = \urls\COLLECTIONS_TEMPLATE[$collection]['table'];
+		$collection = \urls\COLLECTIONS_TEMPLATE[$collection]['source'];
 
 		parent::add($collection);
 	}
 
 	public function update(string $collection) {
-		$collection = \urls\COLLECTIONS_TEMPLATE[$collection]['table'];
+		$collection = \urls\COLLECTIONS_TEMPLATE[$collection]['source'];
 
 		parent::update($collection);
 		
 	}
 
 	public function remove(string $collection) {
-		$collection = \urls\COLLECTIONS_TEMPLATE[$collection]['table'];
+		$collection = \urls\COLLECTIONS_TEMPLATE[$collection]['source'];
 
 		parent::remove($collection);
 	}
